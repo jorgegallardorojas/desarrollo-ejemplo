aa9759d9fa40092a4ef42d5b4aa5da92c4ccb2e7
ZAMUDIO! <0000005979@alumnos.univa.mx>
2026-07-06 01:23:10 -0600

    Reenvio de MAIN no se actualizo porque era en mayusculas y lo puse en minusculas

--- MAIN.C
+++ MAIN.C
@@ -3,5 +3,22 @@
 int main() {
     printf("Hola, GitHub desde C!\n");
     printf("hola jorge gallardo rojas \n");
+
+    //Aquí he comentado yo José Manuel Zamudio Hernández. 
+    //Insertaré un saludo y un pequeño circuito 
+    printf("Hola soy tu compañero de claser!\n");
+    printf("Este es un pequeño circuito de ejemplo...\n");
+    printf("Quieres probar este circuito?  \n");
+    char response[10];
+    scanf("%s", response);
+    if (strcmp(response, "Si") == 0 || strcmp(response, "si") == 0) {
+        printf("¡Genial! Vamos a probarlo.\n");
+        printf("   +---+   \n");
+        printf("   |   |   \n"); 
+        printf("   |   |   \n");
+        printf("   +---+   \n");  
+    } else {
+        printf("Está bien, tal vez en otra ocasión.\n");
+    }
     return 0;
 }
